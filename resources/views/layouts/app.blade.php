527259dbc116285acb11cc1b009a2a47f148dc20
Nesterenko Ivan <ivanviktor93@gmail.com>
2020-11-25 22:46:09 +0400

    MN-41 2урок, дударь. (правки)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/app.css">
-    <script src="js/app.js"></script>
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js"></script>
 </head>
 <body>
     @include('inc.header')
