f8c8b89a6a6d5d3b9929438387f2b611251a1e63
Nesterenko Ivan <ivanviktor93@gmail.com>
2020-11-25 23:58:23 +0400

    MN-42 3ий урок, Дударь. Создание форм на Laravel

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -6,9 +6,25 @@
 
 @section('content')
     <h1>Contacts</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos dolores, eius labore, laborum molestiae
-        mollitia obcaecati omnis praesentium qui, quia recusandae reprehenderit saepe suscipit ut voluptas. Doloremque
-        laboriosam, recusandae. Accusamus aliquid blanditiis culpa eius illo, illum perspiciatis quasi quo. Eaque, eius
-        similique. Adipisci beatae eaque, excepturi ipsam itaque necessitatibus quibusdam.
-    </p>
+
+    <form action="{{ @route('contact-form') }}" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input id="name" type="text" name="name" placeholder="Enter ur name" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input id="email" type="text" name="email" placeholder="Enter ur email" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="subject">Subject</label>
+            <input id="message" type="text" name="subject" placeholder="Enter ur message" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="message">Message</label>
+            <textarea id="message" name="message" placeholder="Enter ur message" class="form-control"></textarea>
+        </div>
+        <button type="submit" class="btn btn-success">Submit</button>
+    </form>
 @endsection
